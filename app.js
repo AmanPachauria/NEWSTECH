023865de2f2169b0d535a8352e297c26fee77c95
AmanPachauria <98804864+AmanPachauria@users.noreply.github.com>
2022-02-02 18:28:51 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,89 @@
+const btn = document.querySelector(".btn");
+  
+// Listen for click event
+btn.onclick = function (e) {
+
+    // Create span element
+    let ripple = document.createElement("span");
+
+    // Add ripple class to span
+    ripple.classList.add("ripple");
+
+    // Add span to the button 
+    this.appendChild(ripple);
+
+    // Get position of X
+    let x = e.clientX - e.target.offsetLeft;
+
+    // Get position of Y 
+    let y = e.clientY - e.target.offsetTop;
+
+    // Position the span element
+    ripple.style.left = `${x}px`;
+    ripple.style.top = `${y}px`;
+
+    // Remove span after 0.3s
+    setTimeout(() => {
+        ripple.remove();
+    }, 300);
+
+};
+
+
+var responsiveSlider = function() {
+
+    var slider = document.getElementById("slider");
+    var sliderWidth = slider.offsetWidth;
+    var slideList = document.getElementById("slideWrap");
+    var count = 1;
+    var items = slideList.querySelectorAll("li").length;
+    var prev = document.getElementById("prev");
+    var next = document.getElementById("next");
+    
+    window.addEventListener('resize', function() {
+      sliderWidth = slider.offsetWidth;
+    });
+    
+    var prevSlide = function() {
+      if(count > 1) {
+        count = count - 2;
+        slideList.style.left = "-" + count * sliderWidth + "px";
+        count++;
+      }
+      else if(count = 1) {
+        count = items - 1;
+        slideList.style.left = "-" + count * sliderWidth + "px";
+        count++;
+      }
+    };
+    
+    var nextSlide = function() {
+      if(count < items) {
+        slideList.style.left = "-" + count * sliderWidth + "px";
+        count++;
+      }
+      else if(count = items) {
+        slideList.style.left = "0px";
+        count = 1;
+      }
+    };
+    
+    next.addEventListener("click", function() {
+      nextSlide();
+    });
+    
+    prev.addEventListener("click", function() {
+      prevSlide();
+    });
+    
+    setInterval(function() {
+      nextSlide()
+    }, 5000);
+    
+    };
+    
+    window.onload = function() {
+    responsiveSlider();  
+    }
+    
+    
